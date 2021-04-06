ab31357b6e5d18399a864e3b68acd49fc6323e6c
slimsamil <samil-gencaslan@gmx.de>
2021-04-06 17:17:05 +0200

    added crud

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Product, Customer, Order
+from .models import *
+from .forms import OrderForm
 # Create your views here.
 
 def home(request):
@@ -19,4 +20,46 @@ def products(request):
 
 def customer(request, pk):
     customer = Customer.objects.get(id=pk)
-    return render(request, 'accounts/customer.html')
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+    context={'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+        
+    context = {'order':order}
+    return render(request, 'accounts/delete.html', context)
